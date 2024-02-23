7eb80fe049b467b71718e93dfceccf490ba45499
PetraCore <102828971+PetraCore@users.noreply.github.com>
2024-02-23 14:11:53 +0100

    Implement "Add new book" feature

--- script.js
+++ script.js
@@ -69,9 +69,36 @@ function generateLibraryHTML() {
     });
 
     htmlCode += `
-        <div class='card addBookBtn'>
+        <div class="card openBookCreatorBtn" id="openBookCreatorBtn">
             <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><g fill="none" fill-rule="evenodd"><path d="M24 0v24H0V0zM12.594 23.258l-.012.002l-.071.035l-.02.004l-.014-.004l-.071-.036c-.01-.003-.019 0-.024.006l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427c-.002-.01-.009-.017-.016-.018m.264-.113l-.014.002l-.184.093l-.01.01l-.003.011l.018.43l.005.012l.008.008l.201.092c.012.004.023 0 .029-.008l.004-.014l-.034-.614c-.003-.012-.01-.02-.02-.022m-.715.002a.023.023 0 0 0-.027.006l-.006.014l-.034.614c0 .012.007.02.017.024l.015-.002l.201-.093l.01-.008l.003-.011l.018-.43l-.003-.012l-.01-.01z"/><path fill="currentColor" d="M9 5a2 2 0 0 1 2-2h2a2 2 0 0 1 2 2v4h4a2 2 0 0 1 2 2v2a2 2 0 0 1-2 2h-4v4a2 2 0 0 1-2 2h-2a2 2 0 0 1-2-2v-4H5a2 2 0 0 1-2-2v-2a2 2 0 0 1 2-2h4zm4 0h-2v5a1 1 0 0 1-1 1H5v2h5a1 1 0 0 1 1 1v5h2v-5a1 1 0 0 1 1-1h5v-2h-5a1 1 0 0 1-1-1z"/></g></svg>
         </div>
+    `;
+
+    htmlCode += `
+        <dialog class="bookCreator" id="bookCreator">
+            <form id="bookCreatorForm" class="card" method="dialog">
+                <div class="options">
+                    <svg id="closeBookCreatorBtn" class="closeBookCreatorBtn" xmlns="http://www.w2.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><g fill="none"><path d="M24 0v24H0V0zM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035c-.01-.004-.019-.001-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427c-.002-.01-.009-.017-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093c.012.004.023 0 .029-.008l.004-.014l-.034-.614c-.003-.012-.01-.02-.02-.022m-.715.002a.023.023 0 0 0-.027.006l-.006.014l-.034.614c0 .012.007.02.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z" /><path fill="currentColor" d="M12 2c5.523 0 10 4.477 10 10s-4.477 10-10 10S2 17.523 2 12S6.477 2 12 2M9.879 8.464a1 1 0 0 0-1.498 1.32l.084.095l2.12 2.12l-2.12 2.122a1 1 0 0 0 1.32 1.498l.094-.083L12 13.414l2.121 2.122a1 1 0 0 0 1.498-1.32l-.083-.095L13.414 12l2.122-2.121a1 1 0 0 0-1.32-1.498l-.095.083L12 10.586z" /></g></svg>
+                </div>
+                <div class="inputWrapper">
+                    <label for="title">Title</label>
+                    <input type="text" name="title" id="title" placeholder="Crime and Punishment" required>
+                </div>
+                <div class="inputWrapper">
+                    <label for="author">Author</label>
+                    <input type="text" name="author" id="author" placeholder="Fyodor Dostoevsky" required>
+                </div>
+                <div class="inputWrapper">
+                    <label for="pages">Pages</label>
+                    <input type="number" name="pages" id="pages" min="0" placeholder="671" required>
+                </div>
+                <div class="inputWrapper">
+                    <label for="read">I finished reading the book</label>
+                    <input type="checkbox" name="read" id="read">
+                </div>
+                <button>Add book</button>
+            </form>
+        </dialog>
     `;
 
     return htmlCode;
@@ -80,6 +107,34 @@ function generateLibraryHTML() {
 function loadLibrary() {
     const libraryContainer = document.querySelector('#library');
     libraryContainer.innerHTML = generateLibraryHTML();
+    activateLibraryButtons();
+}
+
+function activateLibraryButtons() {
+    const bookCreator = document.querySelector('#bookCreator');
+    const openBookCreatorBtn = document.querySelector('#openBookCreatorBtn');
+    const closeBookCreatorBtn = document.querySelector('#closeBookCreatorBtn');
+    const bookCreatorForm = document.querySelector('#bookCreatorForm');
+
+    openBookCreatorBtn.addEventListener('click', () => {
+        bookCreator.showModal();
+    });
+
+    closeBookCreatorBtn.addEventListener('click', () => {
+        bookCreator.close();
+    });
+
+    bookCreatorForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        const title = document.querySelector('#title').value;
+        const author = document.querySelector('#author').value;
+        const pages = document.querySelector('#pages').value;
+        const read = document.querySelector('#read').value;
+
+        addBookToLibrary(title, author, pages, read);
+        loadLibrary();
+    });
 }
 
 loadLibrary();
